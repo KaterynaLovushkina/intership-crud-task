db0aba113fd73bc0b959a64d7bec7bf879f8e57e
KaterynaLovushkina <Kateryna.Lovushkina.IR.2021@lpnu.ua>
2023-09-13 15:10:52 +0300

    Add comments

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -16,62 +16,83 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CrudController extends AbstractController
 {
+    // Main url to open
     #[Route('/', name: 'main')]
     public function index(
         EntityManagerInterface $entityManager,
         Request $request,
         PaginatorInterface $paginator
     ): Response {
+        // Create a new instance of the Crud entity
         $crud = new Crud();
 
+        // Retrieve paginated data and create a form
         $pages = $this->createPagination($entityManager, $request, $paginator);
         $form = $this->createCrudForm($crud);
 
+        // Render the 'crud/index.html.twig' template with data
         return $this->render('crud/index.html.twig', [
             'list' => $pages,
             'form' => $form,
         ]);
     }
 
+    // Method to create new item and add to database
     #[Route('/create', name: 'create')]
     public function create(
         EntityManagerInterface $entityManager,
         Request $request,
         PaginatorInterface $paginator
     ) {
+        // Create a new instance of the Crud entity
         $crud = new Crud();
 
+        // Create a form for CRUD data entry and handle form submission
         $form = $this->createCrudForm($crud);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Save the submitted data to the database
             $entityManager->persist($crud);
             $entityManager->flush();
 
+            // Add a flash message to indicate successful data entry
             $this->addFlash('notice', 'Comment is successfully added');
 
+            // Redirect to the 'main' route after successful submission
             return $this->redirectToRoute('main');
         }
+
+        // Retrieve paginated data
         $pages = $this->createPagination($entityManager, $request, $paginator);
 
+        // Render the 'crud/index.html.twig' template with data and form
         return $this->render('crud/index.html.twig', [
             'list' => $pages,
             'form' => $form->createView(),
         ]);
     }
 
+    // Method to delete item and remove it from database
     #[Route('/delete/{id}', name: 'delete')]
     public function delete(EntityManagerInterface $entityManager, $id): Response
     {
+        // Find the Crud entity by ID
         $crud = $entityManager->getRepository(Crud::class)->find($id);
+
+        // If the entity doesn't exist, throw a NotFoundHttpException
         if (!$crud) {
             throw new NotFoundHttpException('The item does not exist with this id.');
         }
+
+        // Remove the entity from the database
         $entityManager->remove($crud);
         $entityManager->flush();
 
+        // Add a flash message to indicate successful deletion
         $this->addFlash('notice', 'Comment is successfully deleted');
 
+        // Redirect to the 'main' route after successful deletion
         return $this->redirectToRoute('main');
     }
 
@@ -80,6 +101,7 @@ class CrudController extends AbstractController
         Request $request,
         PaginatorInterface $paginator
     ): SlidingPagination {
+        // Paginate the data using KnpPaginator and return the pagination object
         $pagination = $paginator->paginate(
             $entityManager->getRepository(Crud::class)->paginationQuery(),
             $request->query->get('page', 1),
